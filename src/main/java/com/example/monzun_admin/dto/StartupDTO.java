695ae6daca693c9799395716401dfb51748c2998
Coldbwinz <whentheendisgettingcloser@mail.ru>
2020-12-04 20:18:08 +0300

    startup  controller

---
+++ src/main/java/com/example/monzun_admin/dto/StartupDTO.java
@@ -0,0 +1,134 @@
+package com.example.monzun_admin.dto;
+
+import com.example.monzun_admin.model.Startup;
+
+import java.util.Date;
+
+public class StartupDTO {
+    private Long id;
+    private AttachmentShortDTO logo;
+    private UserListDTO owner;
+    private String name;
+    private String description;
+    private String businessPlan;
+    private String tasks;
+    private String growthPlan;
+    private String useArea;
+    private String points;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public StartupDTO() {
+    }
+
+    public StartupDTO(Startup startup) {
+        this.id = startup.getId();
+        this.name = startup.getName();
+        this.owner = new UserListDTO(startup.getOwner());
+        this.logo = new AttachmentShortDTO(startup.getLogo());
+        this.description = startup.getDescription();
+        this.businessPlan = startup.getBusinessPlan();
+        this.tasks = startup.getTasks();
+        this.growthPlan = startup.getGrowthPlan();
+        this.points = startup.getPoints();
+        this.useArea = startup.getUseArea();
+        this.createdAt = startup.getCreatedAt();
+        this.updatedAt = startup.getUpdatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AttachmentShortDTO getLogo() {
+        return logo;
+    }
+
+    public void setLogo(AttachmentShortDTO logo) {
+        this.logo = logo;
+    }
+
+    public UserListDTO getOwner() {
+        return owner;
+    }
+
+    public void setOwner(UserListDTO owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBusinessPlan() {
+        return businessPlan;
+    }
+
+    public void setBusinessPlan(String businessPlan) {
+        this.businessPlan = businessPlan;
+    }
+
+    public String getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(String tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getGrowthPlan() {
+        return growthPlan;
+    }
+
+    public void setGrowthPlan(String growthPlan) {
+        this.growthPlan = growthPlan;
+    }
+
+    public String getUseArea() {
+        return useArea;
+    }
+
+    public void setUseArea(String useArea) {
+        this.useArea = useArea;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/com/example/monzun_admin/dto/StartupListDTO.java
@@ -0,0 +1,54 @@
+package com.example.monzun_admin.dto;
+
+import com.example.monzun_admin.model.Startup;
+
+import java.util.Date;
+
+public class StartupListDTO {
+    private Long id;
+    private String name;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public StartupListDTO() {
+    }
+
+    public StartupListDTO(Startup startup) {
+        this.id = startup.getId();
+        this.name = startup.getName();
+        this.createdAt = startup.getCreatedAt();
+        this.updatedAt = startup.getUpdatedAt();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
