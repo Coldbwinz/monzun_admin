ec2fb6053255fbaea3ce5a3d71acf09174d97b46
Coldbwinz <whentheendisgettingcloser@mail.ru>
2020-12-10 00:32:38 +0300

    optimize imports login controller

--- src/main/java/com/example/monzun_admin/controller/LoginController.java
+++ src/main/java/com/example/monzun_admin/controller/LoginController.java
@@ -16,8 +16,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping("api/auth")
 public class LoginController {
