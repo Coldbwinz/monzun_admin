011656656fd611975169521df4bcc6700ef075dd
Coldbwinz <whentheendisgettingcloser@mail.ru>
2020-12-09 23:30:55 +0300

    cors filter

--- src/main/java/com/example/monzun_admin/configure/SecurityConfigurer.java
+++ src/main/java/com/example/monzun_admin/configure/SecurityConfigurer.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -36,7 +37,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter implements 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf()
+        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class).csrf()
                 .disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/login")
